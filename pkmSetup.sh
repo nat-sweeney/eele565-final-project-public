ac608e49291974c083355f6be485ab6645c27676
unknown <natbsweeney@gmail.com>
2023-12-13 15:29:33 -0700

    Removed old files

--- pkmSetup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load Julia
-
-julia -e "using Pkg; Pkg.activate(\""${ENV_NAME}"\"); Pkg.add(\"LinearAlgebra\"); Pkg.add(\"Clustering\"); Pkg.add(\"MAT\"); Pkg.add(\"BenchmarkTools\"); Pkg.add(\"Plots\");"
